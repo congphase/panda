a29a2e86c23d884f048930d780570df1695aa54a
Hung Le <hungkhanhle@gmail.com>
2021-11-17 15:15:46 +0700

    Refactor safety model ford

--- board/safety/safety_ford.h
+++ board/safety/safety_ford.h
@@ -18,63 +18,48 @@
 #define FORD_BRAKE_SYS_FEATURES       (0x415)
 
 const struct lookup_t FORD_LOOKUP_ANGLE_RATE_UP = {
-    {2., 7., 17.},
-    {5., .8, .15}};
+  {2., 7., 17.},
+  {5., .8, .15}
+};
 
 const struct lookup_t FORD_LOOKUP_ANGLE_RATE_DOWN = {
-    {2., 7., 17.},
-    {5., 3.5, 0.4}};
+  {2., 7., 17.},
+  {5., 3.5, 0.4}
+};
 
 const int FORD_DEG_TO_CAN = 10;
 
-static int ford_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
-
-  int addr = GET_ADDR(to_push);
+static int ford_rx_hook(CAN_FIFOMailBox_TypeDef *to_push)
+{
   int bus = GET_BUS(to_push);
-  bool unsafe_allow_gas = unsafe_mode & UNSAFE_DISABLE_DISENGAGE_ON_GAS;
+  int addr = GET_ADDR(to_push);
 
-  if (addr == FORD_WHEEL_SPEED) {
-    // wheel speeds are 14 bits every 16
-    vehicle_moving = false;
-    for (int i = 0; i < 8; i += 2) {
-      vehicle_moving |= GET_BYTE(to_push, i) | (GET_BYTE(to_push, (int)(i + 1)) & 0xFCU);
+  if (bus == 0)
+  {
+    if (addr == FORD_BRAKE_SYS_FEATURES)
+    {
+      vehicle_speed = ((GET_BYTE(to_push, 0) << 8) | (GET_BYTE(to_push, 1))) * 0.01 / 3.6;
+    }
+
+    if (addr == FORD_CRUISE_STATUS)
+    {
+      int cruise_state = (GET_BYTE(to_push, 1) & 0x7);
+      bool cruise_engaged = (cruise_state != 0) && (cruise_state != 3);
+
+      if (cruise_engaged && !cruise_engaged_prev)
+      {
+        controls_allowed = 1;
+      }
+
+      if (!cruise_engaged)
+      {
+        controls_allowed = 0;
+      }
+
+      cruise_engaged_prev = cruise_engaged;
     }
   }
 
-  // state machine to enter and exit controls
-  if (addr == FORD_STEERING_BUTTONS) {
-    bool cancel = GET_BYTE(to_push, 1) & 0x1;
-    bool set_or_resume = GET_BYTE(to_push, 3) & 0x30;
-    if (cancel) {
-      controls_allowed = 0;
-    }
-    if (set_or_resume) {
-      controls_allowed = 1;
-    }
-  }
-
-  // exit controls on rising edge of brake press or on brake press when
-  // speed > 0
-  if (addr == FORD_CRUISE_STATUS) {
-    brake_pressed = GET_BYTE(to_push, 0) & 0x20;
-    if (brake_pressed && (!brake_pressed_prev || vehicle_moving)) {
-      controls_allowed = 0;
-    }
-    brake_pressed_prev = brake_pressed;
-  }
-
-  // exit controls on rising edge of gas press
-  if (addr == FORD_ENGINE_DATA) {
-    gas_pressed = ((GET_BYTE(to_push, 0) & 0x03) | GET_BYTE(to_push, 1)) != 0;
-    if (!unsafe_allow_gas && gas_pressed && !gas_pressed_prev) {
-      controls_allowed = 0;
-    }
-    gas_pressed_prev = gas_pressed;
-  }
-
-  if ((safety_mode_cnt > RELAY_TRNS_TIMEOUT) && (bus == 0) && (addr == FORD_LKA_CONTROL)) {
-    relay_malfunction_set();
-  }
   return 1;
 }
 
@@ -84,35 +69,19 @@ static int ford_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
 // else
 //     block all commands that produce actuation
 
-static int ford_tx_hook(CAN_FIFOMailBox_TypeDef *to_send) {
-
+static int ford_tx_hook(CAN_FIFOMailBox_TypeDef *to_send)
+{
   int tx = 1;
   int addr = GET_ADDR(to_send);
+  bool violation = false;
 
-  // disallow actuator commands if gas or brake (with vehicle moving) are pressed
-  // and the the latching controls_allowed flag is True
-  int pedal_pressed = brake_pressed_prev && vehicle_moving;
-  bool unsafe_allow_gas = unsafe_mode & UNSAFE_DISABLE_DISENGAGE_ON_GAS;
-  if (!unsafe_allow_gas) {
-    pedal_pressed = pedal_pressed || gas_pressed_prev;
-  }
-  bool current_controls_allowed = controls_allowed && !(pedal_pressed);
-
-  // STEER: safety check
-  if (addr == FORD_LKA_CONTROL) {
-    if (!current_controls_allowed) {
-      // bits 7-4 need to be 0xF to disallow lkas commands
-      if ((GET_BYTE(to_send, 0) & 0xF0) != 0xF0) {
-        tx = 0;
-      }
-    }
-  }
-
-  // ParkAid steering command safety check
-  if(addr == FORD_PARKAID_DATA)
+  if (relay_malfunction)
   {
-    bool violation = false;
+    tx = 0;
+  }
 
+  if (addr == FORD_PARKAID_DATA)
+  {
     // Steering control: (0.1 * val) - 1000 in deg.
     // We use 1/10 deg as a unit here
     int raw_angle_can = (((GET_BYTE(to_send, 2) & 0x7F) << 8) | GET_BYTE(to_send, 3));
@@ -120,7 +89,8 @@ static int ford_tx_hook(CAN_FIFOMailBox_TypeDef *to_send) {
     bool steer_enabled = (GET_BYTE(to_send, 2) >> 7);
 
     // Rate limit check
-    if (controls_allowed && steer_enabled) {
+    if (controls_allowed && steer_enabled)
+    {
       float delta_angle_float;
       delta_angle_float = (interpolate(FORD_LOOKUP_ANGLE_RATE_UP, vehicle_speed) * FORD_DEG_TO_CAN);
       int delta_angle_up = (int)(delta_angle_float) + 1;
@@ -130,47 +100,52 @@ static int ford_tx_hook(CAN_FIFOMailBox_TypeDef *to_send) {
       int lowest_desired_angle = desired_angle_last - ((desired_angle_last >= 0) ? delta_angle_down : delta_angle_up);
       violation |= max_limit_check(desired_angle, highest_desired_angle, lowest_desired_angle);
     }
+
     desired_angle_last = desired_angle;
 
-    if(!controls_allowed && steer_enabled) {
+    if (!controls_allowed && steer_enabled)
+    {
       violation = true;
     }
-
-    if(violation) {
-      tx = 0;
-      controls_allowed = 0;
-    }
   }
 
-  // FORCE CANCEL: safety check only relevant when spamming the cancel button
-  // ensuring that set and resume aren't sent
-  if (addr == FORD_STEERING_BUTTONS) {
-    if ((GET_BYTE(to_send, 3) & 0x30) != 0) {
-      tx = 0;
-    }
+  if (violation)
+  {
+    tx = 0;
+    controls_allowed = 0;
   }
-
-  // 1 allows the message through
   return tx;
 }
 
-// TODO: keep camera on bus 2 and make a fwd_hook
-static int ford_fwd_hook(int bus_num, CAN_FIFOMailBox_TypeDef *to_fwd) {
+static int ford_fwd_hook(int bus_num, CAN_FIFOMailBox_TypeDef *to_fwd)
+{
   int bus_fwd = -1;
   int addr = GET_ADDR(to_fwd);
-  if (!relay_malfunction) {
+
+  if (!relay_malfunction)
+  {
     if ((bus_num == 0) && (addr != FORD_ENG_VEHICLE_SP_THROTTLE) && 
-        (addr != FORD_PARKAID_DATA) && (addr != FORD_BRAKE_SYS_FEATURES)) {
+        (addr != FORD_PARKAID_DATA) && (addr != FORD_BRAKE_SYS_FEATURES))
+    {
       bus_fwd = 2;
-    } else if ((bus_num == 2) && (addr != FORD_LKA_CONTROL) && (addr != FORD_LKA_UI)) {
+    } 
+    else if ((bus_num == 2) && (addr != FORD_LKA_CONTROL) && (addr != FORD_LKA_UI))
+    {
       bus_fwd = 0;
     }
-  }	
+  }
+
   return bus_fwd;
 }
 
+static void ford_init(int16_t param) {
+  UNUSED(param);
+  controls_allowed = 0;
+  relay_malfunction_reset();
+}
+
 const safety_hooks ford_hooks = {
-  .init = nooutput_init,
+  .init = ford_init,
   .rx = ford_rx_hook,
   .tx = ford_tx_hook,
   .tx_lin = nooutput_tx_lin_hook,
