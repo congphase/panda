3f4fcc3ec591a843bd89c1b08358f2f64258c446
Hung Le <hungkhanhle@gmail.com>
2021-11-17 14:47:17 +0700

    Define macros for message id

--- board/safety/safety_ford.h
+++ board/safety/safety_ford.h
@@ -6,6 +6,17 @@
 //      accel rising edge
 //      brake rising edge
 //      brake > 0mph
+
+#define FORD_WHEEL_SPEED              (0x217)
+#define FORD_STEERING_BUTTONS         (0x83)
+#define FORD_CRUISE_STATUS            (0x165)
+#define FORD_ENGINE_DATA              (0x204)
+#define FORD_LKA_CONTROL              (0x3CA)
+#define FORD_LKA_UI                   (0x3D8)
+#define FORD_PARKAID_DATA             (0x3A8)
+#define FORD_ENG_VEHICLE_SP_THROTTLE  (0x202)
+#define FORD_BRAKE_SYS_FEATURES       (0x415)
+
 const struct lookup_t FORD_LOOKUP_ANGLE_RATE_UP = {
     {2., 7., 17.},
     {5., .8, .15}};
@@ -22,7 +33,7 @@ static int ford_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
   int bus = GET_BUS(to_push);
   bool unsafe_allow_gas = unsafe_mode & UNSAFE_DISABLE_DISENGAGE_ON_GAS;
 
-  if (addr == 0x217) {
+  if (addr == FORD_WHEEL_SPEED) {
     // wheel speeds are 14 bits every 16
     vehicle_moving = false;
     for (int i = 0; i < 8; i += 2) {
@@ -31,7 +42,7 @@ static int ford_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
   }
 
   // state machine to enter and exit controls
-  if (addr == 0x83) {
+  if (addr == FORD_STEERING_BUTTONS) {
     bool cancel = GET_BYTE(to_push, 1) & 0x1;
     bool set_or_resume = GET_BYTE(to_push, 3) & 0x30;
     if (cancel) {
@@ -44,7 +55,7 @@ static int ford_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
 
   // exit controls on rising edge of brake press or on brake press when
   // speed > 0
-  if (addr == 0x165) {
+  if (addr == FORD_CRUISE_STATUS) {
     brake_pressed = GET_BYTE(to_push, 0) & 0x20;
     if (brake_pressed && (!brake_pressed_prev || vehicle_moving)) {
       controls_allowed = 0;
@@ -53,7 +64,7 @@ static int ford_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
   }
 
   // exit controls on rising edge of gas press
-  if (addr == 0x204) {
+  if (addr == FORD_ENGINE_DATA) {
     gas_pressed = ((GET_BYTE(to_push, 0) & 0x03) | GET_BYTE(to_push, 1)) != 0;
     if (!unsafe_allow_gas && gas_pressed && !gas_pressed_prev) {
       controls_allowed = 0;
@@ -61,7 +72,7 @@ static int ford_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
     gas_pressed_prev = gas_pressed;
   }
 
-  if ((safety_mode_cnt > RELAY_TRNS_TIMEOUT) && (bus == 0) && (addr == 0x3CA)) {
+  if ((safety_mode_cnt > RELAY_TRNS_TIMEOUT) && (bus == 0) && (addr == FORD_LKA_CONTROL)) {
     relay_malfunction_set();
   }
   return 1;
@@ -88,7 +99,7 @@ static int ford_tx_hook(CAN_FIFOMailBox_TypeDef *to_send) {
   bool current_controls_allowed = controls_allowed && !(pedal_pressed);
 
   // STEER: safety check
-  if (addr == 0x3CA) {
+  if (addr == FORD_LKA_CONTROL) {
     if (!current_controls_allowed) {
       // bits 7-4 need to be 0xF to disallow lkas commands
       if ((GET_BYTE(to_send, 0) & 0xF0) != 0xF0) {
@@ -98,7 +109,7 @@ static int ford_tx_hook(CAN_FIFOMailBox_TypeDef *to_send) {
   }
 
   // ParkAid steering command safety check
-  if(addr == 0x3A8)
+  if(addr == FORD_PARKAID_DATA)
   {
     bool violation = false;
 
@@ -133,7 +144,7 @@ static int ford_tx_hook(CAN_FIFOMailBox_TypeDef *to_send) {
 
   // FORCE CANCEL: safety check only relevant when spamming the cancel button
   // ensuring that set and resume aren't sent
-  if (addr == 0x83) {
+  if (addr == FORD_STEERING_BUTTONS) {
     if ((GET_BYTE(to_send, 3) & 0x30) != 0) {
       tx = 0;
     }
@@ -146,11 +157,12 @@ static int ford_tx_hook(CAN_FIFOMailBox_TypeDef *to_send) {
 // TODO: keep camera on bus 2 and make a fwd_hook
 static int ford_fwd_hook(int bus_num, CAN_FIFOMailBox_TypeDef *to_fwd) {
   int bus_fwd = -1;
-  int addr = GET_ADDR(to_fwd);	
+  int addr = GET_ADDR(to_fwd);
   if (!relay_malfunction) {
-    if ((bus_num == 0) && (addr != 0x202) && (addr != 0x3A8) && (addr != 0x415)) {
+    if ((bus_num == 0) && (addr != FORD_ENG_VEHICLE_SP_THROTTLE) && 
+        (addr != FORD_PARKAID_DATA) && (addr != FORD_BRAKE_SYS_FEATURES)) {
       bus_fwd = 2;
-    } else if ((bus_num == 2) && (addr != 0x3CA) && (addr != 0x3D8)) {
+    } else if ((bus_num == 2) && (addr != FORD_LKA_CONTROL) && (addr != FORD_LKA_UI)) {
       bus_fwd = 0;
     }
   }	
